b8d1837050761d8ed618fb03af7633df24489a7a
Avi Weit <weit@il.ibm.com>
2023-03-03 18:07:36 +0200

    Update main script.

---
+++ ueransim/main-v0.2.py
@@ -0,0 +1,74 @@
+import os
+import re
+import subprocess as sp
+import sys
+import time
+import threading
+
+
+UERANSIM_DIR = '/home/weit/UERANSIM/'
+NR_UE = 'build/nr-ue'
+NR_CLI = 'build/nr-cli'
+
+# relative path to proper configuration file
+config_map = {
+    'gnb0': {
+        'slice0': 'config/ue-gnb1-slice0.yaml',
+        'slice1': 'config/ue-gnb1-slice1.yaml'
+    },
+    'gnb1': {
+        'slice0': 'config/ue-gnb2-slice0.yaml',
+        'slice1': 'config/ue-gnb2-slice1.yaml'
+    }
+}
+
+
+# Connection setup for PDU session[1] is successful, TUN interface[uesimtun0, 10.51.0.3] is up.
+SUCCESS_RE = re.compile('.*is successful, TUN interface\[(\w+), (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\] is up\.')
+
+
+def execute(cmd):
+    print ("ENTER execute: %s" % cmd)
+    popen = sp.Popen(cmd, stdout=sp.PIPE, universal_newlines=True)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line 
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        sys.stdout.write ('EXIT execute [%s]: %s\n' % (return_code, cmd))
+        raise sp.CalledProcessError(return_code, cmd)
+    sys.stdout.write ('EXIT execute [%s]: %s\n' % (return_code, cmd))
+
+
+def single_ue_flow(mcc, mnc, msin):
+    sys.stdout.write('ENTER single_ue_flow %s %s %s \n' % (mcc, mnc, msin))
+    e = execute([UERANSIM_DIR + NR_UE, '-c', UERANSIM_DIR + config_map['gnb0']['slice0'],
+                      '-i', 'imsi-%s%s%s' % (mcc, mnc, msin)])
+    for l in e:
+        matched = SUCCESS_RE.match(l)
+        if (matched):
+            sys.stdout.write('SUCCESSFUL UE: %s, UE ip %s\n' % (msin, matched.group(2)))
+            break
+    time.sleep(2)
+    # TODO: stream data
+    os.system(UERANSIM_DIR + NR_CLI + ' ' + 'imsi-%s%s%s -e \"deregister switch-off\"' % (mcc, mnc, msin))
+    sys.stdout.write('EXIT single_ue_flow %s %s %s \n' % (mcc, mnc, msin))
+    for l in e:
+        pass
+
+
+def main(args=None):
+    single_ue_flow('208', '93', '000000000%d' % 1)
+    # single_ue_flow('208', '93', '000000000%d' % 2)
+    # single_ue_flow('208', '93', '000000000%d' % 3)
+    #
+    # single_ue_flow('208', '93', '000000000%d' % 4)
+    # single_ue_flow('208', '93', '000000000%d' % 5)
+    # single_ue_flow('208', '93', '000000000%d' % 6)
+    #
+    # single_ue_flow('208', '93', '000000000%d' % 7)
+    # single_ue_flow('208', '93', '000000000%d' % 8)
+    # single_ue_flow('208', '93', '000000000%d' % 9)
+   
+if __name__ == "__main__":
+    main()
--- ueransim/main.py
+++ ueransim/main.py
@@ -2,37 +2,45 @@ import argparse
 
 import os
 import subprocess as sp
+import time
+import threading
 
 
-def main(args):
-    if args["mode"] == 'create':
-        os.system('/ueransim/nr-ue -c /ueransim/config/uecfg.yaml -i imsi-208930000000001 &')
-        os.system('/ueransim/nr-ue -c /ueransim/config/uecfg.yaml -i imsi-208930000000002 &')
-        os.system('/ueransim/nr-ue -c /ueransim/config/uecfg.yaml -i imsi-208930000000003 &')
-        os.system('/ueransim/nr-ue -c /ueransim/config/uecfg.yaml -i imsi-208930000000004 &')
-        os.system('/ueransim/nr-ue -c /ueransim/config/uecfg.yaml -i imsi-208930000000005 &')
-        os.system('/ueransim/nr-ue -c /ueransim/config/uecfg.yaml -i imsi-208930000000006 &')
-        os.system('/ueransim/nr-ue -c /ueransim/config/uecfg.yaml -i imsi-208930000000007 &')
-        os.system('/ueransim/nr-ue -c /ueransim/config/uecfg.yaml -i imsi-208930000000008 &')
-        os.system('/ueransim/nr-ue -c /ueransim/config/uecfg.yaml -i imsi-208930000000009 &')
-        os.system('/ueransim/nr-ue -c /ueransim/config/uecfg.yaml -i imsi-208930000000010 &')
+def single_ue_flow(mcc, mnc, msin):
+    x = threading.Thread(target=os.system, args=('/ueransim/nr-ue -c /ueransim/config/uecfg.yaml -i imsi-%s%s%s' % (mcc, mnc, msin), ))
+    x.start()
+    time.sleep(2)
+    # TODO: stream data
+    sp.getoutput('/ueransim/nr-cli imsi-%s%s%s -e \"deregister switch-off\"' % (mcc, mnc, msin))
+    x.join()
 
-    if args["mode"] == "terminate":
-        os.system('/ueransim/nr-cli imsi-208930000000001 -e \"deregister switch-off\"')
-        os.system('/ueransim/nr-cli imsi-208930000000002 -e \"deregister switch-off\"')
-        os.system('/ueransim/nr-cli imsi-208930000000003 -e \"deregister switch-off\"')
-        os.system('/ueransim/nr-cli imsi-208930000000004 -e \"deregister switch-off\"')
-        os.system('/ueransim/nr-cli imsi-208930000000005 -e \"deregister switch-off\"')
-        os.system('/ueransim/nr-cli imsi-208930000000006 -e \"deregister switch-off\"')
-        os.system('/ueransim/nr-cli imsi-208930000000007 -e \"deregister switch-off\"')
-        os.system('/ueransim/nr-cli imsi-208930000000008 -e \"deregister switch-off\"')
-        os.system('/ueransim/nr-cli imsi-208930000000009 -e \"deregister switch-off\"')
-        os.system('/ueransim/nr-cli imsi-208930000000010 -e \"deregister switch-off\"')
-    else:
-        raise Exception("Illegal 'mode' %s" % args["mode"])
+
+def main(args=None):
+    single_ue_flow('208', '93', '000000000%d' % 1)
+    single_ue_flow('208', '93', '000000000%d' % 2)
+    single_ue_flow('208', '93', '000000000%d' % 3)
+
+    single_ue_flow('208', '93', '000000000%d' % 4)
+    single_ue_flow('208', '93', '000000000%d' % 5)
+    single_ue_flow('208', '93', '000000000%d' % 6)
+
+    single_ue_flow('208', '93', '000000000%d' % 7)
+    single_ue_flow('208', '93', '000000000%d' % 8)
+    single_ue_flow('208', '93', '000000000%d' % 9)
+   
+    #
+    # threads = list()
+    # for i in [1 , 2]: #, 3, 4, 5, 6, 7, 8, 9]:
+    #     x = threading.Thread(target=single_ue_flow, args=('208', '93', '000000000%d' % i, ))
+    #     threads.append(x)
+    #     x.start()
+    #
+    # for index, x in enumerate(threads):
+    #     x.join()
+    #     print ("Main    : thread %d done", index)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Description of your program')
-    parser.add_argument('-m','--mode', help='select mode (create/terminate)', required=True)
-    args = vars(parser.parse_args())
-    main(args)
+    # parser = argparse.ArgumentParser(description='Description of your program')
+    # parser.add_argument('-m','--mode', help='select mode (create/terminate)', required=True)
+    # args = vars(parser.parse_args())
+    main()
